641860a9f12336895c83ae544a6c71eeed28c49b
Daniel Nitsche <daniel.nitsche@astro.su.se>
2021-11-08 14:04:55 +0100

    Preserve GET parameters through the paginator

--- frontend/templatetags/pagination_controls.py
+++ frontend/templatetags/pagination_controls.py
@@ -3,6 +3,13 @@ from django import template
 register = template.Library()
 
 
+@register.simple_tag
+def replace_query_param(request, field, value):
+    params = request.GET.copy()
+    params[field] = value
+    return params.urlencode()
+
+
 @register.inclusion_tag('frontend/pagination_controls.html')
-def pagination_controls(paginator, page_obj):
-    return {'paginator': paginator, 'page_obj': page_obj}
+def pagination_controls(request, paginator, page_obj):
+    return {'request': request, 'paginator': paginator, 'page_obj': page_obj}
