1b18f8ceba55108f5145ac9ad52814f17e97cd4e
Daniel Nitsche <daniel.nitsche@astro.su.se>
2021-11-29 16:53:38 +0100

    Extend DataCube ingestion tests

--- ingestion/tests/test_ingest_data_cube.py
+++ ingestion/tests/test_ingest_data_cube.py
@@ -1,18 +1,30 @@
 import os
+import tempfile
+from datetime import datetime
 
 from astropy.io import fits
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from data_access.utils import generate_absolute_path_to_data_cube
 from ingestion.ingesters.ingest_data_cube import ingest_data_cube
 from observations.models import Instrument, DataCube
 from observations.utils.generate_observation_id import generate_observation_id
+from previews.ingesters.ingest_image_previews import THUMBNAIL_WIDTH_PX
+
+"""
+This test requires a science_data/ directory to be accessible in the local filesystem.
+
+Make sure that the SCIENCE_DATA_ROOT is set correctly in the settings for the dev environment.
+"""
 
 
 TEST_FITS_FILE =\
     '2019-04-19/CRISP/nb_6173_2019-04-19T17:34:39_scans=0-4_stokes_corrected_export2021-05-28T15:08:12_im.fits'
 
 
+@override_settings(
+    MEDIA_ROOT=tempfile.gettempdir(),
+)
 class TestIngestDataCube(TestCase):
     def setUp(self):
         Instrument.objects.bulk_create(
@@ -36,6 +48,24 @@ class TestIngestDataCube(TestCase):
         self.assertEqual(cube.instrument, instrument)
         self.assertEqual(cube.path, generate_absolute_path_to_data_cube(TEST_FITS_FILE))
         self.assertEqual(cube.size, size)
+
+        # Compare a small sample of the extracted metadata.
+        self.assertEqual(cube.metadata.date_beg, datetime(2019, 4, 19, 17, 34, 55, 503950))
+        self.assertEqual(cube.metadata.release, '2020-08-05')
+        self.assertEqual(cube.metadata.cname3, 'Wavelength')
+
+        # Check that preview images have been ingested.
+        thumbnail_size = cube.previews.thumbnail.size
+        self.assertGreater(thumbnail_size, 0)
+        self.assertEqual(cube.previews.thumbnail.width, THUMBNAIL_WIDTH_PX)
+
+        full_size_size = cube.previews.full_size.size
+        self.assertGreater(full_size_size, 0)
+        self.assertGreater(full_size_size, thumbnail_size)
+        self.assertGreater(cube.previews.full_size.width, THUMBNAIL_WIDTH_PX)
+
+        # Check that spectral line data preview has been ingested.
+        self.assertGreater(cube.spectral_line_data.data_preview.size, 0)
 
     def test_ingest_missing_file(self):
         fits_file = '/tmp/missing_file.fits'
