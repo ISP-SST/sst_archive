baf79a17d1a13de713eb9764b01dd11ccae3675b
Daniel Nitsche <daniel.nitsche@astro.su.se>
2021-12-06 15:26:38 +0100

    Remove django-allauth social apps from admin interface

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
+from allauth.socialaccount.models import SocialApp, SocialAccount, SocialToken
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
@@ -19,3 +20,10 @@ class UserProfileInlineAdmin(admin.StackedInline):
 
 
 extend_admin(User, UserAdmin, UserProfileInlineAdmin, weight=0)
+
+# Remove the social parts of the django-allauth application from the admin interface.
+# We don't use the social parts, but we can't remove that app completely since there are
+# apparently dependencies from the core part of allauth.
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialToken)
