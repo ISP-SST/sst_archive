7b2fbdde55bb2e16219da55314ac8e262d636d32
Daniel Nitsche <daniel.nitsche@astro.su.se>
2021-12-15 13:55:12 +0100

    Remove unusued files

--- data_access/forms/token_form.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class TokenForm(forms.Form):
-    token = forms.CharField(required=True,
-                            widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'font-family: monospace'}))
